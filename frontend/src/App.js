cc13ed357723cd4d1b81c28b7bee7e0834b19435
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-06-13 17:07:33 +0200

    implementation of react router Dom with react-router-dom library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 
-import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      
-      <div className="container">
-        <Home />
+    <Router>
+      <div className="App">
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
